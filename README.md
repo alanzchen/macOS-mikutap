e92a2b0d84a09db21c6d1c279ef20a9dc152dfc7
Alan Chen <github@zenan.ch>
2020-07-13 13:11:02 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # macOS-mikutap
+
 Ported Mikutap app for macOS. https://aidn.jp/mikutap/
+
+This app will run as your wallpaper and monitor your keystrokes on any other app so you can enjoy your very own Miku song while doing other stuff (such as writing papers).
+
+# Installation
+
+Downlaod the binary in releases and run. Please grant the permission when macOS asks for it and relaunch the app.
+
+# Build from source
+
+Clone this project and run:
+
+```
+yarn install
+yarn dist
+```
+
+# Known Issues
+
+ - App crashes when pressing media keys. Related to https://github.com/wilix-team/iohook/issues/246
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { app, BrowserWindow, screen } = require('electron')
+const ioHook = require('iohook');
+
+function getKeyCode(char) {
+  var keyCode = char.charCodeAt(0);
+  if(keyCode > 90) {  // 90 is keyCode for 'z'
+    return keyCode - 32;
+  }
+  return keyCode;
+}
+
+function createWindow () {
+  // Create the browser window.
+  const { width, height } = screen.getPrimaryDisplay().size
+  console.log(screen.getPrimaryDisplay().size)
+  const win = new BrowserWindow({
+    type: "desktop",
+    width: width,
+    height: height,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: false,
+    skipTaskbar: true,
+    enableLargerThanScreen: true,
+    hasShadow: false
+  })
+  ioHook.on('keypress', ({ keychar }) => {
+    try {
+      var k = String.fromCharCode(keychar)
+      var keycode = getKeyCode(k)
+      win.webContents.send("ping", keycode)
+    } catch (error) {
+      console.log(error)
+    }
+  });
+  // and load the index.html of the app.
+  win.loadFile('aidn.jp/mikutap/index.html')
+  // win.webContents.toggleDevTools()
+  ioHook.start(true)
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
